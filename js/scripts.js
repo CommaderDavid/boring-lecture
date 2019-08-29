66a7a8da996db4d37d481f70c0d96c870b051057
David Boedigheimer <stealth259@gmail.com>
2019-08-29 14:46:47 -0700

    Add click classes and toggleClass function.  Add border class.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("#one").click(function() {
+    $("#one").toggleClass("background-border");
+  });
+
+  $("#two").click(function() {
+    $("#two").toggleClass("background-border");
+  });
+
+  $("#three").click(function() {
+    $("#three").toggleClass("background-border");
+  });
+});
